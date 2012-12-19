c5db39e63b0cfdc9b100939faccc96ec85649184
Dan Palmer <dan.palmer@me.com>
2012-12-19 21:32:26 +0000

    Initial Commit

---
+++ COMP3004-C2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  COMP3004-C2
+//
+//  Created by Dan Palmer on 19/12/2012.
+//  Copyright (c) 2012 Dan Palmer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
